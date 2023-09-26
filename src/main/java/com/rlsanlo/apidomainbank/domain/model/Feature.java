f26d27bb4507a137ecab606f4d5b1d510c0f6972
Ronaldo Lopes <rlsanlo@gmail.com>
2023-09-26 01:04:23 -0300

    feat:first commit

---
+++ src/main/java/com/rlsanlo/apidomainbank/domain/model/Feature.java
@@ -0,0 +1,9 @@
+package com.rlsanlo.apidomainbank.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb.feature")
+public class Feature extends BaseItem {
+}
---
+++ src/main/java/com/rlsanlo/apidomainbank/domain/model/News.java
@@ -0,0 +1,8 @@
+package com.rlsanlo.apidomainbank.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb.news")
+public class News extends BaseItem {
+
+}
